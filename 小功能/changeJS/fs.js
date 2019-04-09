7b76176d91f4d762d10f57d0fe92778d6a20e073
zzb <297234792@qq.com>
2019-04-09 15:33:45 +0800

    test

--- 小功能/changeJS/fs.js
+++ 小功能/changeJS/fs.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
+
 var fs = require("fs");
 require('shelljs/global');
 
-var path = process.argv[2] || './data';
+// var path = process.argv[2] || './data';
+var path = process.argv[2] || '/Users/zzb/work/txbstyle/RN_chinese/ReactNative/sheet/data.js';
 var outPutName = process.argv[3] || 'data.js';
 
+// to do use import
+// import data from '/Users/zzb/work/txbstyle/RN_chinese/ReactNative/sheet/data.js';
+
 console.log('fs start path, outPutName', path, outPutName)
+
 const data = require(path);
 
 const changeData = 'export const sheetData = ' + JSON.stringify(data);
