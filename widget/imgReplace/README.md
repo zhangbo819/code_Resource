e5335ba8c7223390d972acf122fd59fc87dae03a
zzb <297234792@qq.com>
2019-04-10 11:18:45 +0800

    add replace

---
+++ widget/imgReplace/README.md
@@ -0,0 +1,9 @@
+# 用于将智图生成的图替换并删除
+
+## start
+```
+npm i
+sudo npm link
+```
+ 
+
--- widget/imgReplace/index.js
+++ widget/imgReplace/index.js
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
 
+require('shelljs/global');
+
+var path = process.argv[2] || '/Users/zzb/work/txbstyle/RN_chinese/ReactNative/sheet/images';
+
+// cd('./test')
+// cp('-f', './*', '../')
+
+cd(path)
+
+ls().forEach(function (file, index) {
+    // console.log(file)
+    replace(file, index)
+});
+
+function replace(fileName) {
+    cd(fileName)
+
+    if (ls().includes('zhitu-des')) {
+        // console.log(fileName + ' 有')
+
+        cd('zhitu-des')
+
+        cp('-f', './*', '../')
+
+        cd('../')
+
+        rm('-r', 'zhitu-des')
+
+        console.log('文件 ' + fileName + ' 执行成功')
+    } else {
+        console.log('文件 ' + fileName + ' 无')
+    }
+
+    cd('../')
+}
