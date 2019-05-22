50da3cb4d11a737f42af4b4dad4d83a8e061317c
zzb <297234792@qq.com>
2019-05-22 15:02:57 +0800

    zip add Promise&shell&err

--- Node/zip_test/zip.js
+++ Node/zip_test/zip.js
@@ -1,36 +1,65 @@
 var fs = require('fs');
+
 var archiver = require('archiver');
+require('shelljs/global');
 
-const createGzip = ({ filePath, outputPath, fileList }) => {
-    var output = fs.createWriteStream(outputPath);
-    var archive = archiver('zip');
 
-    output.on('close', function () {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
+const createGzipByPromise = ({ filePath, outputPath, fileList }) => {
+    return new Promise((resolve, reject) => {
 
-    archive.on('error', function (err) {
-        console.warn(err)
-        throw err;
-    });
+        var output = fs.createWriteStream(outputPath);
+        var archive = archiver('zip');
+        let status = true;
 
-    archive.pipe(output);
+        output.on('close', function () {
+            // console.log('archiver has been finalized and the output file descriptor has closed.');
+            console.log(`压缩成功, 大小: ${archive.pointer()} 字节, 压缩包路径: ${outputPath}`);
+            resolve(true)
+        });
 
-    fileList.forEach(item => {
-        archive.append(fs.createReadStream(filePath + item), { name: item })
-    });
+        archive.on('error', function (err) {
+            console.warn(err)
+            resolve(false)
+            throw err;
+        });
 
-    // 开始压缩
-    archive.finalize();
+        archive.pipe(output);
+
+        const filePathLs = ls(filePath);
+        fileList.forEach(item => {
+            if (!status) { return; }
+
+            if (item.includes('\/')) {
+                if (item[0] === '\/') {
+                    targetItem = item.slice(1).split('\/')[0];
+                } else {
+                    targetItem = item.split('\/')[0];
+                }
+            } else {
+                targetItem = item;
+            }
+
+            if (filePathLs.includes(targetItem)) {
+                archive.append(fs.createReadStream(filePath + item), { name: item })
+            } else {
+                console.warn(`Error: 文件${filePath + item}不存在`)
+                status = false;
+                resolve(false);
+            }
+        });
+
+        // 开始压缩
+        status && archive.finalize();
+    })
 }
 
-createGzip({
+// test
+createGzipByPromise({
     filePath: __dirname,
     outputPath: __dirname + '/output.zip',
-    fileList: ['/input.txt', '/input1.txt']
+    fileList: ['/input.txt', '/src/input1.txt']
 })
 
 module.exports = {
-    createGzip
+    createGzipByPromise
 }
