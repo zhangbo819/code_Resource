d8695c7520326afaf75f5fbe2645ecb8b7638906
张博 <zhangbo819@163.com>
2019-06-02 16:07:13 +0800

    change

--- Node/zip_test/zip.js
+++ Node/zip_test/zip.js
@@ -3,19 +3,31 @@ var fs = require('fs');
 var archiver = require('archiver');
 require('shelljs/global');
 
+// // test
+// createGzipByPromise({
+//     filePath: __dirname,
+//     outputPath: __dirname + '/output.zip',
+//     fileList: [{ path: '/input.txt' }, { path: '/src', directory: true }]
+// })
 
-const createGzipByPromise = ({ filePath, outputPath, fileList }) => {
+
+function createGzipByPromise({ filePath, outputPath, fileList }) {
     return new Promise((resolve, reject) => {
 
-        var output = fs.createWriteStream(outputPath);
+        var stream = fs.createWriteStream(outputPath);
         var archive = archiver('zip');
         let status = true;
 
-        output.on('close', function () {
+        stream.on('close', function () {
             // console.log('archiver has been finalized and the output file descriptor has closed.');
             console.log(`\n压缩成功, 大小: ${(archive.pointer() / 1000 / 1000).toFixed(1)} MB, 压缩包路径: ${outputPath}`);
             resolve(true)
         });
+
+        // to do console data
+        // archive.on('data', function (data) {
+        //     console.log('data', JSON.stringify(data))
+        // });
 
         archive.on('error', function (err) {
             console.warn(err)
@@ -24,7 +36,7 @@ const createGzipByPromise = ({ filePath, outputPath, fileList }) => {
         });
 
         // to do pipe use console
-        archive.pipe(output);
+        archive.pipe(stream);
 
         const filePathLs = ls(filePath);
         fileList.forEach(({ path, directory = false }) => {
@@ -55,13 +67,6 @@ const createGzipByPromise = ({ filePath, outputPath, fileList }) => {
         status && archive.finalize();
     })
 }
-
-// test
-// createGzipByPromise({
-//     filePath: __dirname,
-//     outputPath: __dirname + '/output.zip',
-//     fileList: [{ path: '/input.txt' }, { path: '/src', directory: true }]
-// })
 
 module.exports = {
     createGzipByPromise
