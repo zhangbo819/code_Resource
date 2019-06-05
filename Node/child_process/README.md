740566e90c5ae46968cc838ee56afde5751f4449
zzb <297234792@qq.com>
2019-06-05 19:11:17 +0800

    add children_proscess

---
+++ Node/child_process/README.md
@@ -0,0 +1,2 @@
+
+[Nodejs进阶：如何玩转子进程（child_process）](https://www.cnblogs.com/chyingp/p/node-learning-guide-child_process.html)
---
+++ Node/child_process/index.js
@@ -0,0 +1,22 @@
+// const spawn = require('child_process').spawn;
+// const ls = spawn('sh', ['index.sh']);
+
+// ls.stdout.on('data', (data) => {
+//     console.log(`stdout: ${data}`);
+// });
+
+// ls.stderr.on('data', (data) => {
+//     console.log(`stderr: ${data}`);
+// });
+
+// ls.on('close', (code) => {
+//     console.log(`child process exited with code ${code}`);
+// });
+var readline = require('readline');
+
+setInterval(() => {
+    // process.stdout.clearLine();
+    // process.stdout.cursorTo(0);
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(Math.random() + '')
+}, 100)
