eff9b4ec210fc02dec362beeedbda1ce84eedf46
zzb <297234792@qq.com>
2019-06-06 18:28:33 +0800

    change

--- widget/rzip/index.js
+++ widget/rzip/index.js
@@ -14,108 +14,121 @@ const {
 } = require("./ruleFile");
 const { rProcess } = require("./rProcess");
 
-const objChoices = {
-    'Do not covered and save the new file': TYPE_OUTPUT,
-    'Covered and save the original file': TYPE_COVER_SAVE,
-    'Directly covered': TYPE_COVER_NOSAVE,
-}
 
-const indexChoices = [
-    'rzip',
-    {
-        name: 'ruhuarn',
-        disabled: 'Temporary does not support'
-    },
-    'ruleFile'
-];
-const choices = [
+const ruleTypeChoices = [
     // new inquirer.Separator(),
-    ...Object.keys(objChoices),
     // new inquirer.Separator(),
-    'Watch targetFile',
-    'Clear output',
-    'Replace the progress bar'
-];
-inquirer.prompt([
     {
-        type: 'list',
-        name: 'index',
-        message: 'Please select a script',
-        choices: indexChoices,
-    },
-    {
-        type: 'list',
-        name: 'ruleType',
-        message: 'Which type do you want to choose?',
-        choices,
-        when: function (answers) {
-            // console.log('answers', answers)
-            return answers.index === indexChoices[2];
+        name: 'Do not covered and save the new file',
+        callback: () => {
+            ruleFileBytype({ type: TYPE_OUTPUT })
         }
     },
-]).then(answers => {
-    console.log(JSON.stringify(answers, null, '  '));
-    const { index } = answers;
-    if (index === indexChoices[0]) {
-        createChildrenProcessBySpawn('sh', [`${__dirname}/rzip.sh`]);
-    } else if (index === indexChoices[1]) {
-        // to do use createChildrenProcessBySpawn
-        // createChildrenProcessBySpawn('sh', [`${__dirname}/ruhuarn.sh`]);
-        child_process.exec(`sh ${__dirname}/ruhuarn.sh`,
-            function (err, stdout, stderr) {
-                if (err) { throw err; }
-                console.log(stdout);
-            }
-        );
-    } else if (index === indexChoices[2]) {
-        ruleFileStart(answers)
-    }
-});
-
-function ruleFileStart(answers) {
-    const { ruleType } = answers;
-    const index = choices.findIndex(i => i === ruleType);
-    const targetDirPath = `${projectPath}sheet`;
-    const outputDir = process.argv[2] || 'output';
-    const targetFile = 'data.js';
-
-    console.log(index, objChoices[ruleType])
-
-    switch (index) {
-        case 0:
-        case 1:
-        case 2:
-            ruleFileStart({ type: objChoices[ruleType] })
-            break;
-        case 3:
+    {
+        name: 'Covered and save the original file',
+        callback: () => {
+            ruleFileBytype({ type: TYPE_COVER_SAVE })
+        }
+    },
+    {
+        name: 'Directly covered',
+        callback: () => {
+            ruleFileBytype({ type: TYPE_COVER_NOSAVE })
+        }
+    },
+    {
+        name: 'Watch targetFile',
+        callback: () => {
             fs.watch(targetDirPath, (eventType, filename) => {
                 if (filename === targetFile) {
                     console.log(`事件类型是: ${eventType}`);
                     ruleFileStart({ type: TYPE_OUTPUT, isNeedclearCache: true })
                 }
             });
-            break;
-        case 4:
+        }
+    },
+    {
+        name: 'Clear output',
+        callback: () => {
             rm('-rf', `${targetDirPath}/${outputDir}`);
-            break;
-        case 5:
+        }
+    },
+    {
+        name: 'Replace the progress bar',
+        callback: () => {
             rProcess({
                 processResourcesPath: '/Users/zzb/work/新进度条/',
                 outputDirPath: targetDirPath + '/images/process/'
             });
-            break;
-    }
+        }
+    },
+];
+const scriptChoices = [
+    {
+        name: 'rzip',
+        callback: () => {
+            createChildrenProcessBySpawn('sh', [`${__dirname}/rzip.sh`]);
+        }
+    },
+    {
+        name: 'ruleFile',
+        callback: inquirerAfter('ruleType', ruleTypeChoices)
+    },
+    {
+        name: 'ruhuarn',
+        disabled: 'Temporary does not support',
+        callback: () => {
+            // to do use createChildrenProcessBySpawn
+            // createChildrenProcessBySpawn('sh', [`${__dirname}/ruhuarn.sh`]);
+            child_process.exec(`sh ${__dirname}/ruhuarn.sh`,
+                function (err, stdout, stderr) {
+                    if (err) { throw err; }
+                    console.log(stdout);
+                }
+            );
+        }
+    },
+];
 
-    function ruleFileStart({ type, isNeedclearCache = false }) {
-        ruleFile({
-            type,
-            targetDirPath,
-            targetFile,
-            outputDir,
-            outputFile: targetFile,
-            isNeedclearCache
-        });
+const targetDirPath = `${projectPath}sheet`;
+const outputDir = process.argv[2] || 'output';
+const targetFile = 'data.js';
+
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'script',
+        message: 'Please select a script',
+        choices: scriptChoices,
+    },
+    {
+        type: 'list',
+        name: 'ruleType',
+        message: 'Which type do you want to choose?',
+        choices: ruleTypeChoices,
+        when: function (answers) {
+            return answers.script === 'ruleFile';
+        }
+    },
+]).then(inquirerAfter('script', scriptChoices));
+
+function inquirerAfter(key, choices) {
+    return function (answers) {
+        console.log(answers[key]);
+        choices.find(({ name }) => name === answers[key]).callback(answers);
     }
+}
+
+function ruleFileBytype({ type, isNeedclearCache = false }) {
+    ruleFile({
+        type,
+        targetDirPath,
+        targetFile,
+        outputDir,
+        outputFile: targetFile,
+        isNeedclearCache
+    });
 }
 
 function createChildrenProcessBySpawn(p1, p2) {
