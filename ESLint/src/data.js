1f3cd9888ad661b6a3c3de5e80b0eaa47b0e5c9c
zzb <297234792@qq.com>
2019-06-06 19:05:35 +0800

    eslint test

---
+++ ESLint/src/data.js
@@ -0,0 +1,61 @@
+export const sheetData = [
+  [
+    {
+      id: 1,
+      type: 25,
+      bgcolor: 'rgba(0,0,0,0)',
+      btnType: 'learn',
+      answer: [
+        [
+          1,
+          3,
+          4
+        ]
+      ],
+      optlist: [
+        {
+          sid: 0,
+          value: 1,
+          txt: '永乐盛世',
+          url: "require('./images/common/card_back.png')",
+          width: 300,
+          height: 493,
+          x: 395,
+          y: 160,
+          pic: "require('./images/card/card11.png')"
+        },
+        {
+          sid: 1,
+          value: 1,
+          txt: '永乐大典',
+          url: "require('./images/common/card_back.png')",
+          width: 300,
+          height: 493,
+          x: 810,
+          y: 160,
+          pic: "require('./images/card/card12.png')"
+        },
+        {
+          sid: 2,
+          value: 1,
+          txt: '郑和下西洋',
+          url: "require('./images/common/card_back.png')",
+          width: 300,
+          height: 493,
+          x: 1225,
+          y: 160,
+          pic: "require('./images/card/card13.png')"
+        }
+      ],
+      btnNext: {
+        text: '下一步',
+        url: "require('./images/common/btn_learn.png')",
+        width: 248,
+        height: 109,
+        x: 836,
+        y: 810
+      }
+    }
+  ]
+]
+export default '1'
