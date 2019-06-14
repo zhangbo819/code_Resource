a6787c90cca3c0fb3bec53fec7f9285b2e8d7cbf
zzb <297234792@qq.com>
2019-06-14 12:02:24 +0800

    fixed .DS_Store & count change

--- widget/dEmpty/index.js
+++ widget/dEmpty/index.js
@@ -2,8 +2,8 @@
 const shelljs = require("shelljs");
 const child_process = require("child_process");
 
-let maxCount = 0;
 let emptyCount = 0;
+let needRmCount = 0;
 
 child_process.exec("find . -mindepth 1 -maxdepth 1 -type d", function (err, stdout, stderr) {
     if (err) throw err;
@@ -12,12 +12,12 @@ child_process.exec("find . -mindepth 1 -maxdepth 1 -type d", function (err, stdo
     stdout.split('\n').forEach(dir => {
         if (!dir) return;
 
-        maxCount++;
-
         child_process.exec(`find ${dir} -mindepth 1 -type f`, function (err, stdout, stderr) {
             if (err) throw err;
-            if (stdout === "") {
+            if (stdout.split('\n').length > 2) return;
+            if (/\.DS_Store/.test(stdout.split('\n')[0]) || stdout === '') {
                 console.log(dir)
+                needRmCount++;
                 shelljs.rm('-rf', dir)
                 emptyCount++;
             }
@@ -26,14 +26,10 @@ child_process.exec("find . -mindepth 1 -maxdepth 1 -type d", function (err, stdo
 });
 
 process.on('exit', function () {
-    console.log(`\nall folders ${maxCount}, empty folders ${emptyCount}\n`);
-    if (maxCount === 0) {
-        console.warn('hint: This path has no folders!\n')
-    } else {
-        if (maxCount > emptyCount) {
-            console.log('hint: The path does not have an empty folder!\n');
-        } else if (maxCount === emptyCount) {
-            console.log('Clear success!\n')
-        }
+    console.log(`\nempty folders ${needRmCount}, cleared folders ${emptyCount}\n`);
+    if (needRmCount === 0) {
+        console.warn('hint: The path does not have an empty folder!\n');
+    } else if (needRmCount === emptyCount) {
+        console.log('Clear success!\n')
     }
 });
