ea638abd34c7aeb2dc465b8f2948444877d38e45
zzb <297234792@qq.com>
2019-06-19 10:22:19 +0800

    add Dep  Watcher &  Vue

--- Vue/kvue/kvue.js
+++ Vue/kvue/kvue.js
@@ -5,6 +5,11 @@ class KVue {
         this.$data = options.data;
 
         this.observe(this.$data);
+
+        new Watcher();
+        this.$data.name;
+        new Watcher();
+        this.$data.msg.age;
     }
 
     observe(value) {
@@ -13,13 +18,17 @@ class KVue {
         }
         Object.keys(value).forEach((key) => {
             this.defineReactive(value, key, value[key]);
-            this.observe(value[key])
         })
     }
 
     defineReactive(obj, key, val) {
+        this.observe(val);
+
+        let dep = new Dep();
+
         Object.defineProperty(obj, key, {
             get() {
+                Dep.target && dep.addDep(Dep.target)
                 return val;
             },
             set(newVal) {
@@ -27,7 +36,8 @@ class KVue {
                     return;
                 }
                 val = newVal;
-                console.log(`${key}更新了：${newVal}`);
+                // console.log(`${key}更新了：${newVal}`);
+                dep.notify();
             }
         })
     }
@@ -35,9 +45,25 @@ class KVue {
 
 
 class Dep {
+    constructor() {
+        this.deps = [];
+    }
 
+    addDep(dep) {
+        this.deps.push(dep)
+    }
+
+    notify() {
+        this.deps.forEach(dep => dep.update())
+    }
 }
 
 class Watcher {
-    
+    constructor() {
+        Dep.target = this;
+    }
+
+    update() {
+        console.log('属性改变了!')
+    }
 }
