eb799a43a7fa3ad16750b762651946f9c92d225f
zzb <>
2019-07-23 10:36:48 +0800

    change

--- widget/rzip/newruhuarn.sh
+++ widget/rzip/newruhuarn.sh
@@ -1,1 +1,1 @@
-sudo /usr/bin/php /Users/zzb/work/qingke_admin/exec/cli_cron.php ruhua.txbapp.cn -template
+sh /Users/zzb/WEB/code_Resource/widget/rzip/password.sh && /usr/bin/php /Users/zzb/work/qingke_admin/exec/cli_cron.php ruhua.txbapp.cn -template
