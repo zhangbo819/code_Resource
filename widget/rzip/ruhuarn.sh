abeb5803fea9f9cf19ec162bb34c88a064feb62c
zzb <>
2019-08-03 13:27:09 +0800

    change

--- widget/rzip/ruhuarn.sh
+++ widget/rzip/ruhuarn.sh
@@ -1,5 +1,5 @@
-sudo /usr/bin/php /Users/zzb/work/txb_admin/exec/cli_cron.php ruhua.txbapp.cn -rn && 
+sh /Users/zzb/WEB/code_Resource/widget/rzip/password.sh && 
+/usr/bin/php /Users/zzb/work/txb_admin/exec/cli_cron.php ruhua.txbapp.cn -rn && 
 # cd /Users/zzb/work/txbstyle/RN_chinese/ReactNative && 
 # find . -name '*.zip' -type f -print -exec rm -i {} \;
-cd /Users/zzb/WEB/code_Resource/widget/rzip &&
-Node ruhuarn.js --type old
+Node /Users/zzb/WEB/code_Resource/widget/rzip/ruhuarn.js --type old
