4bb4e5a8e9279bf44a84606c4d67fba5300a26b1
zzb <>
2019-08-08 11:22:51 +0800

    rm

--- Node/exports_test/change.js
+++
@@ -1,7 +0,0 @@
-var change = function (a) {
-    a = 100;
-    console.log(a)
-}
-
-// exports.change = change;
-module.exports = change;
--- Node/exports_test/index.js
+++
@@ -1,6 +0,0 @@
-var change = require('./change.js');
-console.log(change);
-
-var a = 10;
-change(a);
-console.log(a);
