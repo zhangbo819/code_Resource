731286dc71d79772d37ae0f6042e5547f927cc6d
zzb <>
2019-09-02 11:13:53 +0800

    fixed

--- Node/myServer/index.js
+++ Node/myServer/index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require("url");
+var fs = require('fs');
 
 
 http.createServer(function (request, response) {
@@ -8,18 +9,40 @@ http.createServer(function (request, response) {
     // HTTP 状态值: 200 : OK
     // 内容类型: text/plain
 
-    var urlObj = url.parse(request.url);
-    var pathname = urlObj.pathname;
-    var query = urlObj.query;
+    var { pathname, query } = url.parse(request.url);
 
     console.log("Request for " + pathname + " received." + " query: " + query);
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    // response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    download(query, response)
 
     // 发送响应数据 "Hello World"
-    response.write("Hello World\n");
-    response.end();
+    // response.write("Hello World\n");
+    // response.end();
 }).listen(8888);
 
 // 终端打印如下信息
 console.log('Server running at http://127.0.0.1:8888/');
+
+
+
+function download(query, res) {
+    console.log('download start')
+
+    const fileName = 'test.zip';
+
+    fs.readFile(`./${fileName}`, function (isErr, data) {
+        if (isErr) {
+            res.end("Read file failed!");
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'application/octet-stream', // 告诉浏览器这是一个二进制文件  
+            'Content-Disposition': 'attachment; filename=' + fileName, // 告诉浏览器这是一个需要下载的文件  
+        });
+        res.end(data)
+        console.log('download over')
+    })
+}
