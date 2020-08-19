613416d6695ec24b4d9897de39ce93d05073dcef
张博 <zhangbo819@163.com>
2020-08-19 22:18:42 +0800

    feat(child_process):test

--- Node/child_process/index.js
+++ Node/child_process/index.js
@@ -1,22 +1,24 @@
-// const spawn = require('child_process').spawn;
-// const ls = spawn('sh', ['index.sh']);
+const spawn = require('child_process').spawn;
+const ls = spawn('sh', ['index.sh', '--sss']);
 
-// ls.stdout.on('data', (data) => {
-//     console.log(`stdout: ${data}`);
-// });
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
 
-// ls.stderr.on('data', (data) => {
-//     console.log(`stderr: ${data}`);
-// });
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
 
-// ls.on('close', (code) => {
-//     console.log(`child process exited with code ${code}`);
-// });
-var readline = require('readline');
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
 
-setInterval(() => {
-    // process.stdout.clearLine();
-    // process.stdout.cursorTo(0);
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(Math.random() + '')
-}, 100)
+
+// var readline = require('readline');
+
+// setInterval(() => {
+//     // process.stdout.clearLine();
+//     // process.stdout.cursorTo(0);
+//     readline.cursorTo(process.stdout, 0);
+//     process.stdout.write(Math.random() + '')
+// }, 100)
