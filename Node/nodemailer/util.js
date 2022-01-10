058120208ef20780a68e4831b632e89b857c97c2
zhangbo2021 <zhangbo819@163.com>
2022-01-10 11:44:33 +0800

    feat(node):add fs

--- Node/nodemailer/util.js
+++ Node/nodemailer/util.js
@@ -1,5 +1,6 @@
 const request = require("request")
 const cheerio = require("cheerio")
+const fs = require("fs")
 
 const weatherURL = 'https://tianqi.moji.com/weather/china/beijing/haidian-district'
 
@@ -7,18 +8,33 @@ const greetingURL = 'http://wufazhuce.com/'
 
 // 获取墨迹天气提示信息
 function getWeatherTips(url) {
+    let weatherData
+
+    try {
+        weatherData = require('./weatherData')
+    } catch (err) {
+        console.log('no history')
+    }
+    console.log('weatherData', weatherData)
+
     return new Promise((resolve, reject) => {
         request(weatherURL, (error, res, body) => {
             if (!error) {
-                let html = res.body || "";
-                let $ = cheerio.load(html)
-                let tips = $('.wea_tips em').text().trim()
-                let temp = $('.wea_weather em').text().trim() + '℃'
-                let desc = $('.wea_weather b').text().trim()
-                let water = $('.wea_about span').text().trim()
-                let win = $('.wea_about em').text().trim()
-                let Air = $('.wea_alert em').text().trim()
-                let words = `北京市-海淀区 ${desc}\n温度：${temp}\n空气指数: ${Air}\n风力：${win}\n湿度：${water}\n`
+                const html = res.body || "";
+                const $ = cheerio.load(html)
+                const tips = $('.wea_tips em').text().trim()
+                const temp = $('.wea_weather em').text().trim()
+                const desc = $('.wea_weather b').text().trim()
+                const water = $('.wea_about span').text().trim()
+                const win = $('.wea_about em').text().trim()
+                const Air = $('.wea_alert em').text().trim()
+                const history = `(较昨日${temp > weatherData.temp ? '上升' : '下降'}${Math.abs(temp - weatherData.temp)}度)`
+                const words = `北京市-海淀区 ${desc}\n温度：${temp}℃${history}\n空气指数: ${Air}\n风力：${win}\n湿度：${water}\n`
+
+                // 保留至历史数据
+                fs.writeFileSync('./weatherData.js', 'module.exports = ' + JSON.stringify({
+                    temp,
+                }))
 
                 resolve({
                     tips: `早上好，今日天气提示：${tips}`,
@@ -31,6 +47,8 @@ function getWeatherTips(url) {
     })
 }
 
+// 测试
+// getWeatherTips().then(res => console.log(res))
 
 // 获取wufazhuce.com每日一句
 function getDailyGreeting() {
