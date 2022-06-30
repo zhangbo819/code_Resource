b68ddcd4ad41cbc91d5d7f430c44096e24f09e2e
zhangbo <zhangbo@authing.cn>
2022-06-30 12:00:42 +0800

    feat(send):c

--- widget/WeCom/src/sendByApi.js
+++ widget/WeCom/src/sendByApi.js
@@ -24,7 +24,20 @@ async function AppSend(type = 'markdown') {
     }
   } else if (type === 'markdown') {
     params.markdown = {
-      content: `${getDiffDate()}\n>${data.tips}\n>${data.words}\n\n><font color="info">${data[1]}</font>`
+      content: `${getDiffDate()}\n>${data[0].tips}\n>${data[0].words}\n\n><font color="info">${data[1]}</font>`,
+      // "content": `您的会议室已经预定，稍后会同步到\`邮箱\` 
+      // >**事项详情** 
+      // >事　项：<font color=\"info\">开会</font> 
+      // >组织者：@miglioguan 
+      // >参与者：@miglioguan、@kunliu、@jamdeezhou、@kanexiong、@kisonwang 
+      // > 
+      // >会议室：<font color=\"info\">广州TIT 1楼 301</font> 
+      // >日　期：<font color=\"warning\">2018年5月18日</font> 
+      // >时　间：<font color=\"comment\">上午9:00-11:00</font> 
+      // > 
+      // >请准时参加会议。 
+      // > 
+      // >如需修改会议信息，请点击：[修改会议信息](https://work.weixin.qq.com)`
     }
   }
 
