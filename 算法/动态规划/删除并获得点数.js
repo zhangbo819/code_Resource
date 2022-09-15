426f7fecfe8b00bec4c9aeb8459790d7a934bf3f
zhangbo <zhangbo@authing.cn>
2022-09-15 19:59:28 +0800

    feat: 算法 删除

---
+++ 算法/动态规划/删除并获得点数.js
@@ -0,0 +1,84 @@
+// form https://leetcode.cn/problems/delete-and-earn/?envType=study-plan&id=dong-tai-gui-hua-ru-men
+// 给你一个整数数组 nums ，你可以对它进行一些操作。
+
+// 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
+
+// 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+
+
+// 示例 1：
+// 输入：nums = [3,4,2]
+// 输出：6
+// 解释：
+// 删除 4 获得 4 个点数，因此 3 也被删除。
+// 之后，删除 2 获得 2 个点数。总共获得 6 个点数。
+
+// 示例 2：
+// 输入：nums = [2,2,3,3,3,4]
+// 输出：9
+// 解释：
+// 删除 3 获得 3 个点数，接着要删除两个 2 和 4 。
+// 之后，再次删除 3 获得 3 个点数，再次删除 3 获得 3 个点数。
+// 总共获得 9 个点数。
+
+// var deleteAndEarn = function (nums) {
+//   // 先转成 map
+//   var map = {}
+//   for (let i = 0, len = nums.length; i < len; i++) {
+//     if (map[nums[i]]) {
+//       map[nums[i]] += nums[i]
+//     } else {
+//       map[nums[i]] = nums[i]
+//     }
+//   }
+
+//   console.log(map)
+//   // [[2, 3, 4], [6], [9, 10]];
+
+//   // 然后计算
+//   var dp = [];
+//   var used = {}
+//   for (let num in map) {
+//     const left = map[num - 1] ? map[num - 1] : 0
+//     // const right = map[num + 1] ? map[num + 1] : 0
+
+//     used[num] = true
+
+//     if (map[num] > dp[dp.length - 1] + left) {
+//       dp.push(dp[dp.length - 1] + map[num])
+//     } else {
+      
+//     }
+    
+//     console.log(dp)
+//   }
+//   return dp
+// };
+
+
+
+var deleteAndEarn = function(nums) {
+  let maxVal = 0;
+  for (const val of nums) {
+      maxVal = Math.max(maxVal, val);
+  }
+  const sum = new Array(maxVal + 1).fill(0);
+  for (const val of nums) {
+      sum[val] += val;
+  }
+  console.log('sum', sum)
+  // [ 0, 0, 4, 9, 4 ]
+  return rob(sum);
+};
+
+const rob = (nums) => {
+  const size = nums.length;
+  let first = nums[0], second = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < size; i++) {
+      let temp = second;
+      second = Math.max(first + nums[i], second);
+      first = temp;
+  }
+  return second;
+}
+console.log(deleteAndEarn([2, 2, 3, 3, 3, 4]))
--- 算法/动态规划/打家劫舍.js
+++ 算法/动态规划/打家劫舍.js
@@ -27,10 +27,10 @@ function rob(arr) {
 
   console.log('arr', arr)
   for (var i = 2; i <= arr.length; i++) {
-    // console.log('dp', dp)
+    console.log('dp', dp)
     // 对于每次计算，当前的最优是对比 前1个 与 前2个和现在的和 更大的一个
     dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i - 1])
-    // console.log('i, dp[i]', i, dp[i])
+    console.log('i, dp[i]', i, dp[i])
     // console.log('\n')
   }
 
@@ -38,7 +38,7 @@ function rob(arr) {
 }
 
 
-// console.log(rob([1, 2, 3, 2, 5, 3]))
+console.log(rob([1, 2, 3, 2, 5, 3]))
 // console.log(rob([2, 7, 9, 3, 1]))
 
 module.exports = { rob }
