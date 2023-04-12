ac2e87999dad9516bc2f6ec6225cacb2e3fbfe2d
zhangbo <zhangbo819@163.com>
2023-04-12 15:09:58 +0800

    feat: add Vue3

---
+++ Frame/vue3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>{{ count }}</div>
+  <!-- 不传过去，不会重新渲染  -->
+  <!-- <TestRender /> -->
+  <!-- 传过去就会 -->
+  <TestRender :msg="count" />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import TestRender from "./components/TestRender.vue";
+
+const count = ref(0);
+let timer: any;
+
+onMounted(() => {
+  timer = setInterval(() => {
+    count.value++;
+  }, 2000);
+});
+
+onUnmounted(() => clearInterval(timer));
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ Frame/vue3/src/components/TestRender.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- 穿过去即使不使用，也会重新渲染 -->
+  <!-- <h1>{{ msg }}</h1> -->
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <!-- Date.now() 用来检测是否重新 render -->
+  {{ Date.now() }}
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
