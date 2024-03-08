32b519d80df81f4143a630844ec4733b0abd13fc
zzb2023 <297234792@qq.com>
2024-03-08 17:23:33 +0800

    feat: add

---
+++ 面试题/手写题/事件循环打印.js
@@ -0,0 +1,30 @@
+console.log(1)
+
+async function f1() {
+    await f2()
+    console.log(2)
+}
+
+async function f2() {
+    await console.log(3)
+    console.log(9)
+}
+
+f1()
+
+setTimeout(() => {
+    console.log(7)
+    f2()
+})
+
+new Promise((resolve) => {
+    console.log(4)
+    resolve(5)
+}).then(r => {
+    console.log(r)
+    return 
+}).then(() => {
+    console.log(6)
+})
+
+console.log(8)
