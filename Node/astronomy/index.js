38250191c02eddeddd4c3cc9038c64ddb0b58cbe
zzb2023 <297234792@qq.com>
2026-04-22 18:18:24 +0900

    feat: 占星

---
+++ Node/astronomy/index.js
@@ -0,0 +1,16 @@
+const { planetsHouses, phasePosition } = require("./swisseph");
+
+// 测试数据
+const data = planetsHouses.getData(new Date(), 8);
+
+console.log("基础信息", data);
+console.log(
+  "月亮位置",
+  data.planets.find((i) => i.name === "Moon"),
+);
+
+// const xiangwei = phasePosition.calculateAspects([
+//   ...data.planets,
+//   data.angles.find((i) => i.name === "ASC"), // 只计算上升的相位
+// ]);
+// console.log("相位", xiangwei);
